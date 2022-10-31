58d3b350970f71b58497ee9e7962723b7195e23a
LazerLotus <luan.tsai1991@gmail.com>
2022-11-01 00:25:25 +0800

    feat: login Page html

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const PORT = 3000
+const exphbs = require('express-handlebars')
 
 app.use(routes)
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
 
 app.listen(PORT, () => {
   console.log(`Express is listening on localhost:${PORT}`)
