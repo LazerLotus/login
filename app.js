68fb8bd5ab7b5a302e7547d8f9d5cb768de90003
LazerLotus <luan.tsai1991@gmail.com>
2022-11-02 00:16:50 +0800

    feat: login error and welcome page

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express()
 const routes = require('./routes')
 const PORT = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
-app.use(routes)
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Express is listening on localhost:${PORT}`)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const users = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+
+
+
 
 router.get('/', (req, res) => {
   res.render('home')
+})
+
+router.post('/', (req, res) => {
+  const usermail = req.body.email
+  const password = req.body.password
+  const user = users.filter((user) => user.email === usermail)
+  console.log(user)
+  if (user[0].password === password) {
+    return res.render('welcome', { username: user[0].firstName })
+
+  } else {
+    return res.render('home', { invalid: "invalid" })
+  }
+
+
 })
 
 module.exports = router
